c7ec091fabe430d2afff7ac550bcd0b62f7e00b6
BV003 <bbz975749@gmail.com>
2024-04-29 16:56:53 +0800

    示例代码

---
+++ WebApplication/WebApplication/Services/UserService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApplication.Models;
+
+namespace WebApplication.Services
+{
+     class UserService
+    {
+        private List<User> _users = new List<User>();
+
+        public void AddUser(User user)
+        {
+            if (user == null)
+            {
+                throw new ArgumentNullException(nameof(user), "Cannot add a null user.");
+            }
+
+            _users.Add(user);
+            Console.WriteLine($"User {user.username} added successfully.");
+        }
+
+        public bool RemoveUser(int userId)
+        {
+            var userToRemove = _users.FirstOrDefault(u => u.userId == userId);
+            if (userToRemove == null)
+            {
+                Console.WriteLine("User not found.");
+                return false;
+            }
+
+            _users.Remove(userToRemove);
+            Console.WriteLine($"User with ID {userId} removed successfully.");
+            return true;
+        }
+
+        public User FindUserById(int userId)
+        {
+            return _users.FirstOrDefault(u => u.userId == userId);
+        }
+
+        public IEnumerable<User> GetAllUsers()
+        {
+            return _users;
+        }
+    }
+}
--- WebApplication/WebApplication/obj/Debug/net8.0/WebApplication.AssemblyInfo.cs
+++ WebApplication/WebApplication/obj/Debug/net8.0/WebApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92b86bc2fd7525ae469b5745875f26315a3c9088")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed7e4d7ce2f0abdff12c238ef4dc08ee79900837")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

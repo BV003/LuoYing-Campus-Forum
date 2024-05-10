c591bb0baab1708f9d4612a5c68420229b3f1b36
BV003 <2022302111185@whu.edu.cn>
2024-05-10 17:48:39 +0800

    5/10

--- WebApplication/WebApplication/obj/Debug/net8.0/WebApplication.AssemblyInfo.cs
+++ WebApplication/WebApplication/obj/Debug/net8.0/WebApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed7e4d7ce2f0abdff12c238ef4dc08ee79900837")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+981194f670ebafe50f9fe23d6a189d8166542003")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

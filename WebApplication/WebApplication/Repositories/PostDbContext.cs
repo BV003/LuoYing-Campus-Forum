a978841921dfe2b74a19b5f0921ca12291c8c31a
BV003 <2022302111185@whu.edu.cn>
2024-05-19 16:43:54 +0800

    1

---
+++ WebApplication/WebApplication/Repositories/PostDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Org.BouncyCastle.Asn1.X509;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace softs
+{
+    public class PostDbContext : DbContext
+    {
+        public PostDbContext(DbContextOptions<PostDbContext> options)
+            : base(options)
+        {
+            this.Database.EnsureCreated(); //自动建库建表
+        }
+        public DbSet<Post> Posts { get; set; }
+    }
+}
